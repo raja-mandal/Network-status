83ea5c294de6e1b2adf5118c65269cde1abb867e
raja-mandal <rajamandal706@gmail.com>
2020-01-01 22:02:22 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {fromEvent, Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'detect';
+export class AppComponent implements OnInit, OnDestroy {
+  onlineEvent: Observable<Event>;
+  offlineEvent: Observable<Event>;
+
+  subscriptions: Subscription[] = [];
+
+  connectionStatusMessage: string;
+  connectionStatus: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    /**
+    * Get the online/offline status from browser window
+    */
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
+
+    this.subscriptions.push(this.onlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Back to online';
+      this.connectionStatus = 'online';
+      alert('Online...');
+      //document.write("Back to online");
+    }));
+
+    this.subscriptions.push(this.offlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Connection lost! You are not connected to internet';
+      this.connectionStatus = 'offline';
+      alert('Offline...');
+      //document.write("Connection lost! You are not connected to internet");
+    }));
+  }
+
+  ngOnDestroy(): void {
+    /**
+    * Unsubscribe all subscriptions to avoid memory leak
+    */
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OnlineStatusComponent } from './online-status/online-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnlineStatusComponent
   ],
   imports: [
     BrowserModule
